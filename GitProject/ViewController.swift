d3d560ca9b79cb2f9d9f72effb2345310b8fd188
Jesse By <mister.gansales@yandex.ru>
2019-04-25 13:55:36 +0400

    Initial Commit

---
+++ GitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitProject
+//
+//  Created by Jesse By on 25/04/2019.
+//  Copyright © 2019 Jesse By. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
